d703cec2b975d898d1657de3bd1922d60ba3dff8
iluan <iluan23@hotmail.com>
2019-09-18 12:31:08 -0500

    Add files via upload

---
+++ WRAP0.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
+
+entity WRAP0 is
+	port ( 
+	--inserta aquí tus puertos
+	);
+end;
+architecture synth of WRAP0 is
+--inserta aquí tus señales y componentes
+begin
+--inserta aquí tus mapas de puerto, procesos y conexiones
+end;
